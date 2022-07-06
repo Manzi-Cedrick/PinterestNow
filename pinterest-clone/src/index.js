296d5e1e6b271a16b875c6d1d84c1dbe2f82b9b9
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-06 13:18:28 +0200

    adding redux

--- pinterest-clone/src/index.js
+++ pinterest-clone/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider,createStoreHook } from 'react-redux';
+const root = ReactDOM.createRoot(document.getElementById('root'));7
+const store = createStore(()=>({
+  birds : []
+}))
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
