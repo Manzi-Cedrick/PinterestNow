cff447ffc7aea75099a2dcbfdc0cab4c9c3a454d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-06 13:19:06 +0200

    styling the navbar

--- pinterest-clone/src/components/Home/NavBar.js
+++ pinterest-clone/src/components/Home/NavBar.js
@@ -5,7 +5,7 @@ import Search from './Search'
 import {Link } from 'react-router-dom'
 function NavBar() {
   return (
-    <div className='Navbar sticky top-0 z-10 h-[10vh] flex gap-2 w-full bg-white '>
+    <div className='Navbar sticky top-0 z-10 h-[10vh] flex gap-2 w-screen bg-white'>
         <div className='h-[4vh] w-[4vh] ml-5 mt-5 rounded-full'>
             <img src={logo} alt="" className='h-full w-full rounded-full' />
         </div>
