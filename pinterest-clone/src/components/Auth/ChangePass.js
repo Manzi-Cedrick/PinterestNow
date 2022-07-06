12ed17a5ea45c67d3771250eba8e4ceec618f0f5
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-06 20:25:01 +0200

    change password function optimization

--- pinterest-clone/src/components/Auth/ChangePass.js
+++ pinterest-clone/src/components/Auth/ChangePass.js
@@ -4,7 +4,7 @@ import React,{useState} from 'react'
 import { Link } from 'react-router-dom';
 import { FaFacebook, FaInstagram, FaPinterest } from 'react-icons/fa';
 function ChangePass() {
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState("")
   // const {userToken,setUserToken} = AppContextResume()
 //   const navigate = useNavigate();
   const handleEmail = (event) => {
@@ -32,13 +32,13 @@ function ChangePass() {
       <div className="bg-white relative px-20 drop-shadow-sm h-screen w-full md:m-auto md:w-[80vw] md:px-10">     
       <div className="text-center pt-2 pb-12">
         <i><FaPinterest className="text-[8em] m-auto "/></i>
-        <p>Welcome at Pinterest Community <p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p> </p>
+        <p>Welcome at Pinterest Community </p><p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p> 
       </div>
       <form onSubmit={FormDataSubmission}>
       <p className="font-bold">Forgot your password feel in your email for the verification code send!! </p>
       <div className="flex flex-col gap-2 text-left">
       <label className="font-semibold">email</label>
-      <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onClick={handleEmail} placeholder="Enter your email"/>
+      <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onChange={handleEmail} placeholder="Enter your email"/>
       </div>
       
       <p className="float-right text-[10px]">Don't have account ? <Link to='/signup' className="text-red-500">Signup</Link></p>
