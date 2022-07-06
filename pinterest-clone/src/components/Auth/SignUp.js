902c4d0229d702b1cfb28e6de8757ab08007e496
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-06 20:24:45 +0200

    the Signup page

--- pinterest-clone/src/components/Auth/SignUp.js
+++ pinterest-clone/src/components/Auth/SignUp.js
@@ -3,9 +3,9 @@ import { FaGooglePlay, FaPinterest } from 'react-icons/fa';
 // import axios from 'axios';
 import {  Link } from 'react-router-dom';
 function SignUp() {
-  const [username, setUsername] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   // const navigate = useNavigate();
   const handleUser = (event) => {
     setUsername(event.target.value)
@@ -40,20 +40,20 @@ function SignUp() {
       <div className="bg-white px-20 drop-shadow-sm h-screen w-full md:m-auto md:w-[80vw] md:px-10">     
       <div className="text-center pt-2 pb-12">
         <i><FaPinterest className="text-[8em] m-auto "/></i>
-        <p>Welcome at Pinterest Community <p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p> </p>
+        <p>Welcome at Pinterest Community </p>  <p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p>
       </div>
       <form onSubmit={FormDataSubmission}>
       <div className="flex flex-col gap-2 text-left">
       <label className="font-semibold">username</label>
-      <input type="text" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={username} onClick={handleUser} placeholder="Enter your Name"/>
+      <input type="text" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={username} onChange={handleUser} placeholder="Enter your Name"/>
       </div>
       <div className="flex flex-col gap-2 text-left">
       <label className="font-semibold">email</label>
-      <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onClick={handleEmail} placeholder="Enter your email"/>
+      <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onChange={handleEmail} placeholder="Enter your email"/>
       </div>
       <div className="flex flex-col gap-2 text-left">
       <label className="font-semibold">password</label>
-      <input type="password" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={password} onClick={handlePassword} placeholder="Enter your password"/>
+      <input type="password" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={password} onChange={handlePassword} placeholder="Enter your password"/>
       </div>
       <p className="float-right text-[10px]">Already have account ? <Link to='/login' className="text-red-500">Login Now</Link></p>
       <div className="mt-10">
