1172de9f791db6edcfdecbf5dce184bbca2b6c3c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-08 16:53:25 +0200

    the navigate function and statement

--- pinterest-clone/src/components/Auth/SignUp.js
+++ pinterest-clone/src/components/Auth/SignUp.js
@@ -1,12 +1,12 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { FaGooglePlay, FaPinterest } from 'react-icons/fa';
-// import axios from 'axios';
-import {  Link } from 'react-router-dom';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 function SignUp() {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleUser = (event) => {
     setUsername(event.target.value)
   }
@@ -18,18 +18,27 @@ function SignUp() {
   }
   const FormDataSubmission = (event) => {
     event.preventDefault()
-    // const PostData = async () => {
-    //   const result = await axios.post('http://localhost:8080/v1/auth/user/signup',{
-    //     body: JSON.stringify({
-    //       username: username,
-    //       email: email,
-    //       password: password
-    //     })
-    //   })
-    //   const data = await result.json();
-    //   if(data) return navigate('/login');
-    // }
-    // PostData();
+    const PostData = async () => {
+      try {
+        const result = await fetch('http://localhost:8080/v1/auth/user/signUp',{
+          method : 'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password
+          })
+        })
+        const data = await result.json();
+        console.log("The Data is:", data);
+        if (data) return navigate('/login');
+      } catch (error) {
+        console.log("The Error", error)
+      }
+    }
+    PostData();
     setUsername('')
     setPassword('')
     setEmail('')
@@ -37,39 +46,39 @@ function SignUp() {
   return (
     <div className="h-screen w-screen">
       <img src="" alt="" />
-      <div className="bg-white px-20 drop-shadow-sm h-screen w-full md:m-auto md:w-[80vw] md:px-10">     
-      <div className="text-center pt-2 pb-12">
-        <i><FaPinterest className="text-[8em] m-auto "/></i>
-        <p>Welcome at Pinterest Community </p>  <p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p>
-      </div>
-      <form onSubmit={FormDataSubmission}>
-      <div className="flex flex-col gap-2 text-left">
-      <label className="font-semibold">username</label>
-      <input type="text" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={username} onChange={handleUser} placeholder="Enter your Name"/>
-      </div>
-      <div className="flex flex-col gap-2 text-left">
-      <label className="font-semibold">email</label>
-      <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onChange={handleEmail} placeholder="Enter your email"/>
-      </div>
-      <div className="flex flex-col gap-2 text-left">
-      <label className="font-semibold">password</label>
-      <input type="password" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={password} onChange={handlePassword} placeholder="Enter your password"/>
-      </div>
-      <p className="float-right text-[10px]">Already have account ? <Link to='/login' className="text-red-500">Login Now</Link></p>
-      <div className="mt-10">
-      <button type="submit" className="hover:bg-white hover:text-red-500 border-solid border-red-500 hover:rounded-lg border-2 duration-700  p-5 bg-red-600 w-full h-full px-20 flex justify-center gap-2 text-white font-bold">
-        <FaPinterest className='mt-1'/>Signup
-      </button>
-      </div>
-      </form>
-      <div className="text-center text-[#0000004a]">
-        <span>-or-</span>
-      </div>
-      <div>
-      <button className="p-5 border-green-600 border-solid border-2 w-full h-full px-20 flex justify-center gap-2 text-green-500 font-bold">
-        <FaGooglePlay className='text-yellow-500 mt-1'/>Login with Google
-      </button>
-      </div>
+      <div className="bg-white px-20 drop-shadow-sm h-screen w-full md:m-auto md:w-[80vw] md:px-10">
+        <div className="text-center pt-2 pb-12">
+          <i><FaPinterest className="text-[8em] m-auto " /></i>
+          <p>Welcome at Pinterest Community </p>  <p className="text-[10px]"> Welcome Now at pinterest connection. Establish</p>
+        </div>
+        <form onSubmit={FormDataSubmission}>
+          <div className="flex flex-col gap-2 text-left">
+            <label className="font-semibold">username</label>
+            <input type="text" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={username} onChange={handleUser} placeholder="Enter your Name" />
+          </div>
+          <div className="flex flex-col gap-2 text-left">
+            <label className="font-semibold">email</label>
+            <input type="email" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={email} onChange={handleEmail} placeholder="Enter your email" />
+          </div>
+          <div className="flex flex-col gap-2 text-left">
+            <label className="font-semibold">password</label>
+            <input type="password" className="p-4 drop-shadow-sm bg-slate-50 outline-violet-500 border-none outline-2" value={password} onChange={handlePassword} placeholder="Enter your password" />
+          </div>
+          <p className="float-right text-[10px]">Already have account ? <Link to='/login' className="text-red-500">Login Now</Link></p>
+          <div className="mt-10">
+            <button type="submit" className="hover:bg-white hover:text-red-500 border-solid border-red-500 hover:rounded-lg border-2 duration-700  p-5 bg-red-600 w-full h-full px-20 flex justify-center gap-2 text-white font-bold">
+              <FaPinterest className='mt-1' />Signup
+            </button>
+          </div>
+        </form>
+        <div className="text-center text-[#0000004a]">
+          <span>-or-</span>
+        </div>
+        <div>
+          <button className="p-5 border-green-600 border-solid border-2 w-full h-full px-20 flex justify-center gap-2 text-green-500 font-bold">
+            <FaGooglePlay className='text-yellow-500 mt-1' />Login with Google
+          </button>
+        </div>
       </div>
     </div>
   )
