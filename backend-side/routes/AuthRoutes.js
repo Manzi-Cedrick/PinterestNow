757e10c299747f7e2919b96aff07651fb582fddd
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-08 16:52:49 +0200

    the reset and search user functionality

--- backend-side/routes/AuthRoutes.js
+++ backend-side/routes/AuthRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {Register,LoginNow} = require('../controllers/UserController.js');
+const {Register,LoginNow,ResetPassword,SearchUser} = require('../controllers/UserController.js');
 router.post('/signUp',Register);
 router.post('/login',LoginNow);
+router.post('/resetpassword',ResetPassword);
+router.post('/searchUser',SearchUser);
 module.exports = router ;
