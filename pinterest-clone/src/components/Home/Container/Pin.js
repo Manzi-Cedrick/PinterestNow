e5d7e9c4b66adead4ff253b7ac840663fc12a817
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-08 16:54:02 +0200

    removed the icon in preview

--- pinterest-clone/src/components/Home/Container/Pin.js
+++ pinterest-clone/src/components/Home/Container/Pin.js
@@ -1,6 +1,5 @@
 import React,{useState,useEffect} from 'react'
 import './pic.css';
-import {FaComment} from 'react-icons/fa'
 import './data';
 import Data from './data';
 import CrazyLoader from './CrazyLoader';
@@ -26,25 +25,20 @@ function Pin() {
     return (
         <>
     {lazyloader ? <CrazyLoader/> : 
-    <div className='w-full max-w-full mt-  pb-10 mb-10 gap-5 columns-4 px-2 space-y-5'>
+    <div className='w-[100vw] max-w-full mt-5 gap-5 columns-4 px-4 space-y-5'>
     {img_obj.map((imgobject,index)=>(
         <div key={index} className='card-grid relative overflow-hidden rounded-xl '>
-        <Link to='/pin/preview'>
+        <Link to={`/pin/preview/${imgobject.id}`}>
             <img src={`${imgobject.urls.regular}`} alt=""  />
             <div className='absolute right-2 opacity-0 save-btn duration-700 top-2'>
             <button className='btn bg-red-500 drop-shadow text-bold text-white p-3 rounded-full px-8 flex justify-center place-items-center'>Save</button>
             </div>
             <div className='absolute opacity-0 lower-section duration-700 bottom-2 left-2 flex gap-12'>
             <div className='bg-white flex flex-col justify-center place-items-center rounded-xl '>
-                <a href='/' className='text-underline text-[10px] px-2 decoration-black'>{imgobject.alt_description ? imgobject.alt_description : "www.tiktok.com"}</a>
+                <a href='/' className='text-underline text-[10px] font-bold px-2 decoration-black'>{imgobject.alt_description ? imgobject.alt_description : "www.tiktok.com"}</a>
             </div>
             <div className='flex gap-2'>
-            <div className='h-[6vh] w-[6vh] p-3 flex justify-center place-items-center bg-slate-100 hover:cursor-pointer  rounded-full'>
-                <FaComment className='text-[#808080cf] text-2xl'/>
-            </div>
-            <div className='h-[6vh] w-[6vh] p-3 flex justify-center place-items-center bg-slate-100 hover:cursor-pointer  rounded-full'>
-                <FaComment className='text-[#808080cf] text-2xl'/>
-            </div>
+
             </div>
             </div>
         </Link>
