bfce92feab40c2a678761890da87e5a8bb0322c0
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-09 13:14:23 +0200

    the login testing and frontend connect

--- backend-side/controllers/UserController.js
+++ backend-side/controllers/UserController.js
@@ -16,7 +16,7 @@ const Register = async (req,res)=>{
     let hashpassword = await HashPassword(user_register.password);
     user_register.password = hashpassword;
     const finalUser = await user_register.save();
-    res.status(200).send({success:"ok",register: finalUser});
+    res.status(200).json({success:"ok",register: finalUser});
 }catch(error){
     res.status(400);
     throw new Error(error.message);
@@ -24,18 +24,17 @@ const Register = async (req,res)=>{
 }
 const LoginNow = async (req,res) =>{
     try{
-        const check_User = await User.findOne({email : req.body.email});
+        const check_User = await User.findOne({email : req.body.email });
         if(!check_User){
-            res.status(401);
-            res.send('User Does not exist')
+            res.status(400);
         }
         const validPassword = await bcrypt.compare(req.body.password,check_User.password);
-        if(!validPassword) {
-            res.status(401);
-            res.send('password wrong');
+        if(validPassword) {
+            const user_token = await jwt.sign({id:check_User.id},process.env.secret_key,{expiresIn: '24h'});
+            res.status(200).json({success : "ok",content: check_User ,token : user_token})
+        }else{
+            res.status(400).json({success: "false",message:"User not defined and bad credentials"})
         }
-        const user_token = await jwt.sign({id:check_User.id},process.env.secret_key,{expiresIn: '24h'});
-        res.status(200).json({success : "ok",content: check_User ,token : user_token})
     }catch(error){
         res.status(400)
         throw new Error(error.message);
