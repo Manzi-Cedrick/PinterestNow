ce7ed92a1d7e4a9b46224f2549b94b8a9c392eec
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-10 00:54:45 +0200

    models Pin

--- backend-side/models/PinModel.js
+++ backend-side/models/PinModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const PinSchema = mongoose.Schema({
+    // _id:{type:String,unique:true},
     user_pin_maker: { type: mongoose.Schema.Types.ObjectId , ref : 'User' },
-    link:[{type:Object,required:true}],
+    link:[{type:Object,required:true,unique:true}],
     title:{type:String,default:"cedrick Pin Posting"},
     description:{type:String,default:"cedrick Pin Posting"},
     dominant_color:{type:String},
