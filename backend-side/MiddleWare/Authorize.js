0904da03680ac9517a1a5b970e8ec2fbe116d611
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-21 00:53:40 +0200

    testing token

--- backend-side/MiddleWare/Authorize.js
+++ backend-side/MiddleWare/Authorize.js
@@ -5,7 +5,6 @@ const AuthorizeUser = async (req, res, next) => {
     if(req.headers['authorization'] || req.headers['authorization'].startWith('Bearer ')){
         try{
             const token = req.headers['authorization'].split(" ")[1];
-            console.log(token);
             const decodedInfo = await jwt.decode(token,process.env.secret_key);
             req.user = await User.findById(decodedInfo.id);
             next()
