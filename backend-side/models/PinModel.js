813f6f3f0a8ff976fa64d74c87ebbb4fce729284
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-21 00:52:13 +0200

    removed the mongo id

--- backend-side/models/PinModel.js
+++ backend-side/models/PinModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const PinSchema = mongoose.Schema({
-    // _id:{type:String,unique:true},
     user_pin_maker: { type: mongoose.Schema.Types.ObjectId , ref : 'User' },
-    link:[{type:Object,required:true,unique:true}],
+    link: [{type:Object,required:true,unique:true}],
     title:{type:String,default:"cedrick Pin Posting"},
     description:{type:String,default:"cedrick Pin Posting"},
     dominant_color:{type:String},
+    // id:{type:String,unique:false},
     alt_text:{type:String,default:"cedrick Pin Posting"},
     board_id:{type:String,default:""},
 })
